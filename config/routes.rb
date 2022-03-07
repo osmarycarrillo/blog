ce41dbe1a7f2704bae7036a0bd2a46e4dcdf2d35
Osmary Cecilia Carrillo <Osmary.Carrillo@ocean.ibm.com>
2022-03-07 15:24:22 -0300

    app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "home/index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "articles/new", to: "articles#new";
+  get "articles/2", to: "articles#show";
+  post "articles", to: "articles#create";
 end
