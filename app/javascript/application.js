e8064739f17ad00ac50b2022aba8298c1b288bd7
Osmary Cecilia Carrillo <Osmary.Carrillo@ocean.ibm.com>
2022-03-07 14:56:31 -0300

    Blog

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+//archivos importados por mi
+import 'bootstrap'
+import 'css/styles'
